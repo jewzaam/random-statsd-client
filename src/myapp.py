d4efc4e0cdcc77b9b8397b4781d03eb13fa739a4
Naveen Malik <nmalik@redhat.com>
2022-07-26 17:32:52 -0400

    Trying to get log output to show up from the pod
    Minor tweak to be really sure that output is stdout.

--- src/myapp.py
+++ src/myapp.py
@@ -2,6 +2,7 @@ import statsd
 import time
 import random
 import os
+import sys
 
 server = os.getenv('STATSD_SERVER', default="localhost")
 port = os.getenv('STATSD_PORT', default=8125)
@@ -11,7 +12,7 @@ c = statsd.StatsClient(server, port)
 
 while True:
     r = random.randint(0,1000)
-    print("setting 'random' to: ", r)
+    print("setting 'random' to: {}".format(r), file=sys.stdout)
     c.gauge('random', r)
     time.sleep(sleep_s)
 
