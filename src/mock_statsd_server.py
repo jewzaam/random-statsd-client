ee5a36131d9ff55da6146aba48744320f431dd08
Naveen Malik <nmalik@redhat.com>
2022-07-27 14:35:56 -0400

    TCP version, does not handle server being absent

--- src/mock_statsd_server.py
+++ src/mock_statsd_server.py
@@ -4,10 +4,17 @@ import os
 server = os.getenv('STATSD_SERVER', default="localhost")
 port = os.getenv('STATSD_PORT', default=8125)
 
-sock = socket.socket(socket.AF_INET, # Internet
-                 socket.SOCK_DGRAM) # UDP
+#sock = socket.socket(socket.AF_INET, # Internet
+#                 socket.SOCK_DGRAM) # UDP
+sock = socket.socket() # TCP
 sock.bind((server, port))
+sock.listen(5)
 
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("received message: ", data.strip())
+    (clientSocket, clientAddress) = sock.accept()
+    while True:
+        data = clientSocket.recv(1024) # buffer size is 1024 bytes
+        if len(data) == 0:
+            break
+        print("received message: ", data.strip())
+
--- src/myapp.py
+++ src/myapp.py
@@ -8,11 +8,11 @@ server = os.getenv('STATSD_SERVER', default="localhost")
 port = os.getenv('STATSD_PORT', default=8125)
 sleep_s = int(os.getenv('SLEEP_SECONDS', default=5))
 
-c = statsd.StatsClient(server, port)
+c = statsd.TCPStatsClient(server, port)
 
 while True:
     r = random.randint(0,1000)
-    print("setting 'random' to: {}".format(r), file=sys.stdout, flush=True)
+    print(f"random:{r}|g", flush=True)
     c.gauge('random', r)
     time.sleep(sleep_s)
 
